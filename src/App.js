4174f8e979db08fdf4897a108b85604adb54637a
Stefanie Dziadkowiec <stefaniemarie.dz@gmail.com>
2021-05-21 17:55:05 +0200

    pushing example project to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Navigation from "./components/Navigation";
+const apiKey = "7QyQRTxSHUKkC3AmiVLFfTP384kbf6";
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null);
+  const [inputText, setInputText] = useState();
+  const [city, setCity] = useState("oslo");
+
+  function fetchWeatherData() {
+    fetch(`https://goweather.herokuapp.com/weather/${city}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setWeatherData(data);
+      })
+      .catch((error) => {
+        console.log("something went wrong", error);
+      });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("this is our city:", inputText);
+    setCity(inputText);
+  }
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, [city]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <h1>My Website</h1>
+        <h2>Search for a city:</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="city name"
+            onChange={(e) => setInputText(e.target.value)}
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+
+        {weatherData && (
+          <article>
+            <h2>Today's Weather for {city}:</h2>
+            <ul>
+              <li>Temperature: {weatherData.temperature}</li>
+              <li>Description: {weatherData.description}</li>
+              <li>
+                Tomorrow: {weatherData.forecast[0].temperature}, wind:
+                {weatherData.forecast[0].wind}
+              </li>
+              <li>In Two Days: {weatherData.forecast[1].temperature}</li>
+              <li>In Three Days: {weatherData.forecast[2].temperature}</li>
+            </ul>
+          </article>
+        )}
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>home</li>
+          <li>about</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navigation;
